1291dbc86fa075c2e161cd37d0c0f7e2a96d5e29
Emilie <emilie.chnvt@gmail.com>
2025-04-14 20:53:51 +0200

    method index for Tshir created

--- src/Controller/TshirtController.php
+++ src/Controller/TshirtController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TshirtRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class TshirtController extends AbstractController
 {
     #[Route('/tshirt', name: 'app_tshirt')]
-    public function index(): Response
+    public function index(TshirtRepository $tshirtRepository): Response
     {
+        $tshirts = $tshirtRepository->findAll();
         return $this->render('tshirt/index.html.twig', [
-            'controller_name' => 'TshirtController',
+            'tshirts' => $tshirts,
         ]);
     }
 }
