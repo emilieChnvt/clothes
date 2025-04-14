f30aba21f221c9f689fca297738da3313f8ce3d8
Emilie <emilie.chnvt@gmail.com>
2025-04-14 21:02:11 +0200

    method delete for Tshirt created

--- src/Controller/TshirtController.php
+++ src/Controller/TshirtController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Tshirt;
 use App\Repository\TshirtRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -25,6 +26,15 @@ final class TshirtController extends AbstractController
         return $this->render('tshirt/show.html.twig', [
             'tshirt' => $tshirt,
         ]);
+    }
+
+    #[Route('/tshirt/delete/{id}', name: 'tshirt_delete')]
+    public function delete(Tshirt $tshirt, TshirtRepository $tshirtRepository, EntityManagerInterface $manager): Response
+    {
+        $manager->remove($tshirt);
+        $manager->flush();
+        return $this->redirectToRoute('app_tshirt');
+
     }
 
 }
